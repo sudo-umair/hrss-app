9ac096d584fc7ca5b2daf5db3cff7a4bf85aea3e
Muhammad Umair <muhammadumair523@gmail.com>
2022-09-19 21:31:27 +0500

    minor changes

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import "react-native-gesture-handler";
 import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { store } from "./store/store";
 import { Provider } from "react-redux";
 import Navigator from "./navigators/Navigator";
@@ -8,13 +7,14 @@ import registerNNPushToken from "native-notify";
 import { GLOBALS } from "./utilities/constants/config";
 import FlashMessage from "react-native-flash-message";
 import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
 
 export default function App() {
   const { appId, appToken } = GLOBALS;
   registerNNPushToken(appId, appToken);
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <View style={{ flex: 1 }}>
       <Provider store={store}>
         <Navigator />
       </Provider>
@@ -24,8 +24,8 @@ export default function App() {
         duration={2500}
         animated={true}
         position="top"
-        style={{ zIndex: 100, paddingTop: "7%" }}
+        style={{ zIndex: 100, paddingTop: "10%" }}
       />
-    </SafeAreaView>
+    </View>
   );
 }
