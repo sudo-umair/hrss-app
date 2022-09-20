4f52ad8f9e929dd80bf5d36c410fb2cae5ebdcb0
Muhammad Umair <muhammadumair523@gmail.com>
2022-09-20 13:42:55 +0500

    new native notify acc

--- utilities/constants/config.js
+++ utilities/constants/config.js
@@ -1,8 +1,8 @@
 export const GLOBALS = {
-  appId: 3686,
-  appToken: "bSmfQdmZN8TAxKjrJdk7Px",
-  //
-  //
+  // appId: 3686,
+  // appToken: "bSmfQdmZN8TAxKjrJdk7Px",
+  appId: 3975,
+  appToken: "KDddBoWdQRdHhgp2D2wR2r",
   BASE_URL: "https://hrss-server.herokuapp.com",
   // BASE_URL: "http://192.168.100.3:4400",
   // BASE_URL: "http://192.168.212.117:4400",
