9329ae7101d0ac378c707579e1fbb504f7121ee1
Muhammad Umair <muhammadumair523@gmail.com>
2022-10-23 17:13:18 +0500

    update NN config

--- utilities/constants/config.js
+++ utilities/constants/config.js
@@ -1,8 +1,6 @@
 export const GLOBALS = {
-  // appId: 3686,
-  // appToken: "bSmfQdmZN8TAxKjrJdk7Px",
-  appId: 3975,
-  appToken: 'KDddBoWdQRdHhgp2D2wR2r',
+  appId: 4493,
+  appToken: 'cKld3b9BpMji38mIQiUrbE',
   BASE_URL: 'https://hrss-server.herokuapp.com',
   // BASE_URL: 'http://192.168.100.3:4400',
   // BASE_URL: "http://192.168.212.117:4400",
