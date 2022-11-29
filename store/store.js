3302273311480287df14f03fd56caae4b774081d
Muhammad Umair <muhammadumair523@gmail.com>
2022-11-29 22:37:19 +0500

    removed reducers

--- store/store.js
+++ store/store.js
@@ -1,13 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './user';
-import resourcesReducer from './resources';
-import volunteersReducer from './volunteers';
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
-    resources: resourcesReducer,
-    volunteers: volunteersReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
