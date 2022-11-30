469e07bb0664a1c278dee0b4a817357d3217ddb8
Muhammad Umair <muhammadumair523@gmail.com>
2022-11-30 20:49:31 +0500

    changed animation for app loading

--- screens/LoadingScreen.js
+++ screens/LoadingScreen.js
@@ -1,28 +1,26 @@
-import { View, Text, StyleSheet } from "react-native";
-import { ImageBackground } from "react-native";
-import React from "react";
-import LottieView from "lottie-react-native";
-import SplashScreenImage from "../assets/splash.png";
-import AppLoadingAnimation from "../assets/animations/app-loading.json";
-import { useRef } from "react";
+import { View, Text, StyleSheet } from 'react-native';
+import { ImageBackground } from 'react-native';
+import React from 'react';
+import LottieView from 'lottie-react-native';
+import SplashScreenImage from '../assets/splash.png';
+import AppLoadingAnimation from '../assets/animations/app-loading.json';
+import { useRef } from 'react';
 
 export default function LoadingScreen() {
   const animation = useRef(null);
   return (
     <ImageBackground
       source={SplashScreenImage}
-      resizeMode="cover"
+      resizeMode='cover'
       style={styles.rootContainer}
     >
       <View style={styles.container}>
-        <Text style={styles.text}>App Is Loading, Please Wait.</Text>
         <LottieView
           loop={true}
           autoPlay={true}
           ref={animation}
           style={{
-            width: 150,
-            marginTop: "-10%",
+            width: '50%',
           }}
           source={AppLoadingAnimation}
         />
@@ -34,17 +32,19 @@ export default function LoadingScreen() {
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   container: {
-    marginTop: "100%",
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    position: 'absolute',
+    bottom: '10%',
   },
   text: {
     fontSize: 18,
-    textAlign: "center",
+    textAlign: 'center',
     margin: 10,
   },
 });
