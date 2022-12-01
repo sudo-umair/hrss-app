caeadf013c600a8740b7a1f9ebe8b4e78060dc8a
Muhammad Umair <muhammadumair523@gmail.com>
2022-12-01 15:40:15 +0500

    minor update

--- components/HomeScreen/BottomDisplay.js
+++ components/HomeScreen/BottomDisplay.js
@@ -1,63 +1,63 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
-import BottomDisplayButton from "./BottomDisplayButton";
-import { GlobalStyles as gs } from "../../utilities/constants/styles";
-import { useNavigation } from "@react-navigation/native";
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import BottomDisplayButton from './BottomDisplayButton';
+import { GlobalStyles as gs } from '../../utilities/constants/styles';
+import { useNavigation } from '@react-navigation/native';
 
 export default function BottomDisplay() {
   const navigation = useNavigation();
 
   const goToAccountScreen = () => {
-    navigation.navigate("Account");
+    navigation.navigate('Account');
   };
 
   const goToResourcesScreen = () => {
-    navigation.navigate("Resources");
+    navigation.navigate('Resources');
   };
 
   const goToDontationsScreen = () => {
-    navigation.navigate("Donations");
+    navigation.navigate('Donations');
   };
 
   const goToVolunteersScreen = () => {
-    navigation.navigate("Volunteers");
+    navigation.navigate('Volunteers');
   };
 
   return (
     <View style={styles.container}>
       <View style={styles.buttonContainer}>
         <BottomDisplayButton
-          name="news"
+          name='news'
           size={24}
           color={gs.colors.primary}
-          lib="e"
-          title="Resources"
+          lib='e'
+          title='Resources'
           onPress={goToResourcesScreen}
         />
         <BottomDisplayButton
-          name="volunteer-activism"
+          name='volunteer-activism'
           size={24}
           color={gs.colors.primary}
-          title="Volunteer"
-          lib="m"
+          title='Volunteer'
+          lib='m'
           onPress={goToVolunteersScreen}
         />
       </View>
       <View style={styles.buttonContainer}>
         <BottomDisplayButton
-          name="heart-plus"
+          name='heart-plus'
           size={24}
           color={gs.colors.primary}
-          title="Donatations"
-          lib="mc"
+          title='Donatations'
+          lib='mc'
           onPress={goToDontationsScreen}
         />
         <BottomDisplayButton
-          lib="mc"
-          name="account-circle"
+          lib='mc'
+          name='account-circle'
           size={24}
           color={gs.colors.primary}
-          title="Profile"
+          title='Account'
           onPress={goToAccountScreen}
         />
       </View>
@@ -68,13 +68,13 @@ export default function BottomDisplay() {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: gs.colors.background,
-    marginTop: "10%",
-    alignItems: "center",
-    justifyContent: "center",
+    marginTop: '10%',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   buttonContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
