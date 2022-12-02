0d5cf7ee3145e8c754ffc363d4688f103a34e5c4
Muhammad Umair <muhammadumair523@gmail.com>
2022-12-02 19:01:12 +0500

    moved to vercel

--- utilities/constants/config.js
+++ utilities/constants/config.js
@@ -1,7 +1,7 @@
 export const GLOBALS = {
   appId: 4493,
   appToken: 'cKld3b9BpMji38mIQiUrbE',
-  BASE_URL: 'https://share-and-care-server.herokuapp.com',
+  BASE_URL: 'https://share-and-care-server.vercel.app',
   // BASE_URL: 'http://192.168.100.3:4400',
   // BASE_URL: "http://192.168.212.117:4400",
 };
