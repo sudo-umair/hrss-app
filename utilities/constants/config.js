9a4547215dc9b3d04ae7319205619db162f97ce5
Muhammad Umair <muhammadumair523@gmail.com>
2022-12-11 22:22:37 +0500

    switched to heroku

--- utilities/constants/config.js
+++ utilities/constants/config.js
@@ -3,7 +3,8 @@ export const GLOBALS = {
   appToken: 'AC8kvmfgzQJT72ZKWstWWt',
   FCMServerKey:
     'AAAAvEhbX9E:APA91bE-F5Tt5MskCuvqEvJhRyEmmOlkvhEHJMaJD-AZrWynVTD2vGK1-nvtUxHo0XpgDkvjKh4ioPgQDzYpbNATmNuC-KT8izqImx0Oq92KICNZ5JhH2QncIzzRb48zzSnub3-W1kjH',
-  BASE_URL: 'https://share-and-care-server.vercel.app',
+  BASE_URL: 'https://share-and-care-server.herokuapp.com',
+  // BASE_URL: 'https://share-and-care-server.vercel.app',
   // BASE_URL: 'http://192.168.100.3:4400',
   // BASE_URL: "http://192.168.212.117:4400",
 };
